94e8993f79f30a73b2c87b0f6b69048b06b3e928
JKeyla <joselin.cano@sptech.school>
2024-10-14 02:55:39 -0300

    subindo 2ª parte da dashboard com gráficos e kpi's

--- public/dashboard/dashboardTopico.js
+++ public/dashboard/dashboardTopico.js
@@ -24,7 +24,7 @@ modos.addEventListener("click", () => {
 			"#FFFFFF"
 		);
 		document.documentElement.style.setProperty(
-			"--background-topics",
+			"--background-comentario",
 			"#4C4C4C"
 		);
 
@@ -51,7 +51,7 @@ modos.addEventListener("click", () => {
 			"#343434"
 		);
 		document.documentElement.style.setProperty(
-			"--background-topics",
+			"--background-comentario",
 			"#E3E3E3"
 		);
 
@@ -72,142 +72,16 @@ modos.addEventListener("click", () => {
 
 // Função para criar o gráfico com base no valor atual de textoGrafico
 function criarGrafico() {
-	JSC.Chart("feedbacks-chart", {
-		debug: true,
-		type: "horizontal column solid",
-		title_label_text:
-			"<span style='font-family: 'Montserrat'; font-weight: 600;'>Feedbacks de Janeiro</span>",
-		title_label_style: {
-			fontSize: 24,
-			color: textoGrafico,
-		},
-		title_position: "center",
-		legend_visible: false,
-		box_fill: "none",
-		yAxis: {
-			defaultTick_label_style: { color: "#C3C3C3" },
-			defaultTick: {
-				gridLine_color: "#5D5D5D",
-				line: { color: "#5D5D5D" },
-				gridLine: {
-					dashStyle: "Dash",
-				},
-			},
-		},
-		xAxis: {
-			defaultTick_label_style: { color: textoGrafico },
-			defaultTick: {
-				rangeMode: "curly",
-				line_radius: 10,
-				line: { color: "transparent" },
-				label_padding: 5,
-				label_placement: "outside",
-			},
-		},
-		series: [
-			{
-				points: [
-					{
-						name: "Positivo",
-						y: 200,
-						color: "rgba(153, 255, 153, 0.57)",
-						outline: { color: "#99FF99", width: 3 },
-					},
-					{
-						name: "Neutro",
-						y: 344,
-						color: "rgba(255, 255, 153, 0.57)",
-						outline: { color: "#FFFF99", width: 3 },
-					},
-					{
-						name: "Negativo",
-						y: 112,
-						color: "rgba(255, 153, 153, 0.57)",
-						outline: { color: "#FF9999", width: 3 },
-					},
-				],
-			},
-		],
-	});
-
-	// Avaliações (Em Estrelas)
-	// JSC.Chart("ratings-chart", {
-	// 	debug: true,
-	// 	type: "column solid",
-	// 	title_label_text:
-	// 		"<span style='font-family: 'Montserrat'; font-weight: 600;'>Avaliações (Em Estrelas)</span>",
-	// 	title_label_style: {
-	// 		fontSize: 20,
-	// 		color: textoGrafico,
-	// 	},
-	// 	title_position: "center",
-	// 	legend_visible: false,
-	// 	box_fill: "none",
-	// 	xAxis: {
-	// 		label_color: textoGrafico,
-	// 		defaultTick: {
-	// 			label_style: {
-	// 				color: textoGrafico,
-	// 			},
-	// 		},
-	// 	},
-	// 	yAxis: {
-	// 		defaultTick_label_style: { color: "transparent" },
-	// 		defaultTick: {
-	// 			gridLine_color: "#5D5D5D",
-	// 			line: { color: "#5D5D5D" },
-	// 			gridLine: {
-	// 				dashStyle: "Dash",
-	// 			},
-	// 		},
-	// 		alternateGridFill: "none",
-	// 	},
-	// 	series: [
-	// 		{
-	// 			points: [
-	// 				{
-	// 					name: "1",
-	// 					y: 200,
-	// 					color: "rgba(255, 153, 153, 0.57)",
-	// 					outline: { color: "#FF9999", width: 3 },
-	// 				},
-	// 				{
-	// 					name: "2",
-	// 					y: 344,
-	// 					color: "rgba(255, 204, 153, 0.57)",
-	// 					outline: { color: "#FFCC99", width: 3 },
-	// 				},
-	// 				{
-	// 					name: "3",
-	// 					y: 112,
-	// 					color: "rgba(255, 255, 153, 0.57)",
-	// 					outline: { color: "#FFFF99", width: 3 },
-	// 				},
-	// 				{
-	// 					name: "4",
-	// 					y: 626,
-	// 					color: "rgba(204, 255, 153, 0.57)",
-	// 					outline: { color: "#CCFF99", width: 3 },
-	// 				},
-	// 				{
-	// 					name: "5",
-	// 					y: 626,
-	// 					color: "rgba(153, 255, 153, 0.57)",
-	// 					outline: { color: "#99FF99", width: 3 },
-	// 				},
-	// 			],
-	// 		},
-	// 	],
-	// });
-
+	
 	// Índice de Satisfação
+	// JS 
 	JSC.chart("satisfaction-index", {
 		debug: true,
 		type: "gauge ",
 		legend_visible: false,
 		chartArea_boxVisible: false,
 		title_label_text:
-			"<span style='font-family: 'Montserrat'; font-weight: 600;text-align: center;'>Índice de <br> Satisfação</span>",
+			"",
 		title_label_style: {
 			fontSize: 20,
 			color: textoGrafico,
@@ -251,7 +125,7 @@ function criarGrafico() {
 				type: "marker",
 				name: "Score",
 				shape_label: {
-					text: "720<br/> <span style='fontSize: 24px;'>Bom!</span>",
+					text: "560<br/> <span style= 'font-family: 'Montserrat'; fontSize: 18px; '>Regular!</span>",
 					style: { fontSize: 24, color: textoGrafico },
 				},
 				defaultPoint: {
@@ -267,103 +141,12 @@ function criarGrafico() {
 						size: 15,
 					},
 				},
-				points: [[1, 720]],
+				points: [[1, 560]],
 			},
 		],
 	});
 
-	// Mapa das Lojas
-// 	var capitalNames = [
-// 		"TX 78750",
-// 		"PA 18322",
-// 		"NY 11367",
-// 		"NY 10001",
-// 		"NY 10003",
-// 		"NY 10038",
-// 		"DC 20004",
-// 		"CA 90025",
-// 		"NY 10002",
-// 		"CA 92111",
-// 		"NY 10013",
-// 		"NY 10001-2",
-// 		"CA 94086",
-// 		"FL 32837",
-// 		"UT 84106",
-// 		"DC 20036",
-// 		"CA 90021",
-// 		"FL 33139",
-// 		"CA 92109",
-// 		"NY 12919",
-// 		"CA 90044",
-// 		"CA 90405",
-// 		"FL 32819",
-// 		"TX 78257",
-// 		"IL 60302",
-// 		"TX 78724",
-// 		"CA 91602",
-// 		"NJ 08043",
-// 		"TX 76262",
-// 		"NJ 07104",
-// 		"TX 76177",
-// 		"NV 89119",
-// 		"FL 34746",
-// 		"FL 32730",
-// 		"FL 32819-2",
-// 		"VA 22003",
-// 		"TX 75203",
-// 		"FL 33137",
-// 	];
 
-// 	var capitalValues = [
-// 		50, 70, 90, 40, 60, 20, 80, 30, 55, 75, 85, 65, 95, 15, 25, 45, 10, 35, 20,
-// 		75, 65, 30, 50, 80, 90, 40, 60, 70, 30, 80, 20, 10, 15, 25, 35, 90, 55, 45,
-// 	];
-
-// 	JSC.chart("map-chart", {
-// 		debug: true,
-// 		type: "map",
-// 		legend_visible: false,
-// 		toolbar_position: "inside bottom right",
-// 		title_label_text:
-// 			"<span style='font-family: 'Montserrat'; font-weight: 600;'>Mapa das Lojas</span>",
-// 		title_label_style: {
-// 			fontSize: 24,
-// 			color: textoGrafico,
-// 		},
-// 		title_position: "center",
-// 		box_fill: "none",
-// 		mapping: {
-// 			referenceLayers: "./UsCapitals.json",
-// 			base: {
-// 				layers: "us",
-// 			},
-// 		},
-// 		defaultSeries_shape_padding: 0.23,
-// 		palette: {
-// 			pointValue: function (p) {
-// 				return p.options("z");
-// 			},
-// 			invert: true,
-// 			colors: ["#00e700", "#fefe20", "#ffa500", "#ff0000", "#8b0000"],
-// 		},
-// 		series: [
-// 			{
-// 				id: "bubbleSer",
-// 				name: "Capitals",
-// 				type: "marker",
-// 				opacity: 0.9,
-// 				defaultPoint_marker: {
-// 					type: "material/maps/place",
-// 					size: 25,
-// 					offset: "1,-10",
-// 				},
-// 				points: capitalNames.map(function (n, index) {
-// 					var mapId = "UsCapitals." + n;
-// 					return { map: mapId, z: capitalValues[index] };
-// 				}),
-// 			},
-// 		],
-// 	});
 }
 
 // Chama o gráfico pela primeira vez ao carregar a página
