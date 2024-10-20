ede7ae37a109243f57b1216dbf84b47856e69868
AnilmarChoque <anilmarorellana@gmail.com>
2024-10-20 13:10:46 -0300

    projeto java com conexão ao banco de dados local

---
+++ conexao-bd-java/src/main/java/datamind/DBConnectionProvider.java
@@ -0,0 +1,25 @@
+package datamind;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+public class DBConnectionProvider {
+
+    private final DataSource dataSource;
+
+    public DBConnectionProvider() {
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl("jdbc:mysql://localhost:3306/datamind");
+        basicDataSource.setUsername("root");
+        basicDataSource.setPassword("F4m1l145!");
+        basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+        this.dataSource = basicDataSource;
+    }
+
+    public JdbcTemplate getConnection() {
+        return new JdbcTemplate(dataSource);
+    }
+}
